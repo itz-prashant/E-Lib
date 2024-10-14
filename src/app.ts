0605245f8552c7a950d65434fb38dbd95d34c9b4
Prashant gupta <prashantguptaofficial007@gmail.com>
2024-10-14 16:07:21 +0530

    create user router

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express, { NextFunction, Request, Response } from 'express'
 import globalErrorHandler from './middlewares/globalErrorHandler'
+import userRouter from './user/userRouter'
 
 const app = express()
 
 app.get('/', (req: Request, res: Response, next:NextFunction) => {
     res.json({message: "Welcome to elib apis"})
 })
+
+app.use('/api/users',userRouter)
 
 app.use(globalErrorHandler)
 
