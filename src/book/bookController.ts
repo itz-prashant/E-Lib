a3a03d35898a2703acdcf80e6a2bbd9a58079ddf
Prashant gupta <prashantguptaofficial007@gmail.com>
2024-10-16 16:47:54 +0530

    Add single book end point

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -152,4 +152,20 @@ const listBook = async (req: Request, res: Response, next: NextFunction)=>{
 
 }
 
-export {createBook, updateBook, listBook}
+const getSingleBook = async (req: Request, res: Response, next: NextFunction)=>{
+
+    const bookId = req.params.bookId
+    try {
+        const book = await bookModel.findOne({_id: bookId});
+        if(!book){
+            return next(createHttpError(404, "Book not found"))
+        }
+        res.json(book);
+
+    } catch (error) {
+        return next(createHttpError(500, "Error while getting a book"))
+    }
+
+}
+
+export {createBook, updateBook, listBook, getSingleBook}
